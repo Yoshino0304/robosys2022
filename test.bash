2ceb9d35f6d5c752286aa04eb39d113ccfc0a6fa
Taiki Yoshino <s21C1137QR@s.chibakoudai.jp>
2022-11-29 19:19:55 +0900

    s

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2022 Taiki Yoshino
-#SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2022 Taiki Yoshino
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
